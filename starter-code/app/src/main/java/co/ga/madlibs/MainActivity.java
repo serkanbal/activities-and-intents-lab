260d720c5740adf8cb4fe1546b2d623535a9ed5f
serkanbal <serkanbaltr@gmail.com>
2016-10-15 12:52:29 -0400

    Mostly done, will have to review

--- starter-code/app/src/main/java/co/ga/madlibs/MainActivity.java
+++ starter-code/app/src/main/java/co/ga/madlibs/MainActivity.java
@@ -1,12 +1,86 @@
 package co.ga.madlibs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import static co.ga.madlibs.R.id.animals;
+import static co.ga.madlibs.R.id.game;
+import static co.ga.madlibs.R.id.noun1;
+import static co.ga.madlibs.R.id.noun2;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private EditText mAdjective1;
+    private EditText mAdjective2;
+    private EditText mNoun1;
+    private EditText mNoun2;
+    private EditText mAnimals;
+    private EditText mGame;
+    private Button mButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAdjective1 = (EditText) findViewById(R.id.adjective1);
+        mAdjective2 = (EditText) findViewById(R.id.adjective2);
+        mNoun1 = (EditText) findViewById(noun1);
+        mNoun2 = (EditText) findViewById(noun2);
+        mAnimals = (EditText) findViewById(animals);
+        mGame = (EditText) findViewById(game);
+        mButton = (Button) findViewById(R.id.buttonsurprise);
+
+        mButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+
+        EditText adj1 = (EditText) findViewById(R.id.adjective1);
+        String adj1input = adj1.getText().toString();
+        if (adj1input.isEmpty()) {
+            adj1.setError("Please fill");
+        }
+        EditText adj2 = (EditText) findViewById(R.id.adjective2);
+        String adj2input = adj2.getText().toString();
+        if (adj2input.isEmpty()) {
+            adj2.setError("Please fill");
+        }
+        EditText noun1 = (EditText) findViewById(R.id.noun1);
+        String noun1input = noun1.getText().toString();
+        if (noun1input.isEmpty()) {
+            noun1.setError("Please fill");
+        }
+        EditText noun2 = (EditText) findViewById(R.id.noun2);
+        String noun2input = noun2.getText().toString();
+        if (noun2input.isEmpty()) {
+            noun2.setError("Please fill");
+        }
+        EditText animals = (EditText) findViewById(R.id.animals);
+        String animalsinput = animals.getText().toString();
+        if (animalsinput.isEmpty()) {
+            animals.setError("Please fill");
+        }
+        EditText game = (EditText) findViewById(R.id.game);
+        String gameinput = game.getText().toString();
+        if (gameinput.isEmpty()) {
+            game.setError("Please fill");
+        }
+
+        intent.putExtra("ADJ1", adj1input);
+        intent.putExtra("ADJ2", adj2input);
+        intent.putExtra("NOUN1", noun1input);
+        intent.putExtra("NOUN2", noun2input);
+        intent.putExtra("ANIMALS", animalsinput);
+        intent.putExtra("GAME", gameinput);
+        if (!adj1input.isEmpty() && !adj2input.isEmpty() && !noun1input.isEmpty() &&
+                !noun2input.isEmpty() && !animalsinput.isEmpty() && !gameinput.isEmpty()) {
+            startActivity(intent);
+        }
     }
 }
--- starter-code/app/src/main/java/co/ga/madlibs/ResultActivity.java
+++ starter-code/app/src/main/java/co/ga/madlibs/ResultActivity.java
@@ -1,26 +1,39 @@
 package co.ga.madlibs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Html;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 /**
  * Created by james on 12/7/15.
  */
 public class ResultActivity extends AppCompatActivity {
     private Button mBackButton;
+    private TextView mText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
+        Intent intent = getIntent();
+        String adj1 = intent.getStringExtra("ADJ1");
+        String adj2 = intent.getStringExtra("ADJ2");
+        String noun1 = intent.getStringExtra("NOUN1");
+        String noun2 = intent.getStringExtra("NOUN2");
+        String animals = intent.getStringExtra("ANIMALS");
+        String game = intent.getStringExtra("GAME");
+
         mBackButton = (Button) findViewById(R.id.back_button);
+        mText = (TextView) findViewById(R.id.result_textview);
+
+        mText.setText("A vacation is when you take a trip to a " + adj1 + " place with your " +
+        adj2 + " family. Usually you go to a place that is near " + noun1 + " or up on a " +
+        noun2 + ". A good vacation place is where you can ride " + animals + " or play " +
+        game + ".");
 
         mBackButton.setOnClickListener(new View.OnClickListener() {
             @Override
